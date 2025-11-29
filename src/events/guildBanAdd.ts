38d004b0db45301689ee01c635be92237beade9b
Ozzer0 <ozzer0111@gmail.com>
2025-11-29 17:30:59 -0600

    Fix ban reason

--- src/events/guildBanAdd.ts
+++ src/events/guildBanAdd.ts
@@ -2,7 +2,7 @@ import { createEvent } from 'seyfert';
  
 export default createEvent({
   data: { name: 'guildBanAdd' },
-  run(user, client) {
-    client.messages.write(`${process.env.LOGS_CHANNEL}`, {content: `${user.user.username} has been banned.\nReason: ${client.bans.fetch(user.guildId, user.user.id).then((GuildBan) => GuildBan.reason?.toString())}`});
+  async run(user, client) {
+    client.messages.write(`${process.env.LOGS_CHANNEL}`, {content: `${user.user.username} has been banned.\nReason: ${await client.bans.fetch(user.guildId, user.user.id).then((GuildBan) => GuildBan.reason?.toString())}`});
   }
 })
